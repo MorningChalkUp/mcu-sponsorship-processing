908f167e88b405f2c1c6a73e90caaf8587d6f557
Eric Sherred <eric@ericsherred.com>
2018-11-03 12:42:35 -0500

    single day handling

--- js/sponsor-mcu.js
+++ js/sponsor-mcu.js
@@ -9,25 +9,46 @@ var paid = 0;
 
 (function($){
   var week_ids = [];
+  var day_ids = []
   $.each($('.purchase-checkbox'), function(i, val) {
-    week_ids.push($(val).data('id'));
+    if($(val).hasClass('single-day')) {
+      day_ids.push($(val).data('id'));
+    } else {
+      week_ids.push($(val).data('id'));
+    }
   });
 
   $('.purchase-checkbox').change(function() {
     if ($(this).prop('checked') && $('.cart-item').length < max_weeks) {
-      html = `
-        <div class="cart-item ${$(this).data('id')}" data-price="${$(this).data('price')}" data-item_total="${$(this).data('price')}" data-id="${$(this).data('id')}">
-          <h4>${$(this).data('range')} <span class="price">$${$(this).data('price')}</span></h4>
-          <div class="inside">
-            <p>${$(this).data('notes')}</p>
-            <h5>Add-ons:</h5>
-            <ul class="add-ons">
-              <li><input type="checkbox" data-id="${$(this).data('id')}" class="facebook" data-price="250" id="facebook-${$(this).data('id')}"/> <label for="facebook-${$(this).data('id')}">Facebook Retargeting</label> <span class="price">+ $250</span></li>
-              <li><input type="checkbox" data-id="${$(this).data('id')}" class="ab" data-price="250" id="ab-${$(this).data('id')}"/> <label for="ab-${$(this).data('id')}">A/B Testing</label> <span class="price">+ $250</span></li>
-              <li><input type="checkbox" data-id="${$(this).data('id')}" class="wewrite" data-price="250" id="wewrite-${$(this).data('id')}"/> <label for="wewrite-${$(this).data('id')}">We Write Your Ads</label> <span class="price">+ $250</span></li>
-            </ul>
-          </div>
-        </div>`
+      if($(this).hasClass('single-day')) {
+        html = `
+          <div class="cart-item single-day ${$(this).data('id')}" data-price="${$(this).data('price')}" data-item_total="${$(this).data('price')}" data-id="${$(this).data('id')}">
+            <h4>${$(this).data('range')} <span class="price">$${$(this).data('price')}</span></h4>
+            <div class="inside">
+              <p>${$(this).data('notes')}</p>
+              <h5>Add-ons:</h5>
+              <ul class="add-ons">
+                <li><input type="checkbox" data-id="${$(this).data('id')}" class="ab" data-price="250" id="ab-${$(this).data('id')}"/> <label for="ab-${$(this).data('id')}">A/B Testing</label> <span class="price">+ $250</span></li>
+                <li><input type="checkbox" data-id="${$(this).data('id')}" class="wewrite" data-price="250" id="wewrite-${$(this).data('id')}"/> <label for="wewrite-${$(this).data('id')}">We Write Your Ads</label> <span class="price">+ $250</span></li>
+              </ul>
+            </div>
+          </div>`;
+      } else {
+        html = `
+          <div class="cart-item ${$(this).data('id')}" data-price="${$(this).data('price')}" data-item_total="${$(this).data('price')}" data-id="${$(this).data('id')}">
+            <h4>${$(this).data('range')} <span class="price">$${$(this).data('price')}</span></h4>
+            <div class="inside">
+              <p>${$(this).data('notes')}</p>
+              <h5>Add-ons:</h5>
+              <ul class="add-ons">
+                <li><input type="checkbox" data-id="${$(this).data('id')}" class="facebook" data-price="250" id="facebook-${$(this).data('id')}"/> <label for="facebook-${$(this).data('id')}">Facebook Retargeting</label> <span class="price">+ $250</span></li>
+                <li><input type="checkbox" data-id="${$(this).data('id')}" class="ab" data-price="250" id="ab-${$(this).data('id')}"/> <label for="ab-${$(this).data('id')}">A/B Testing</label> <span class="price">+ $250</span></li>
+                <li><input type="checkbox" data-id="${$(this).data('id')}" class="wewrite" data-price="250" id="wewrite-${$(this).data('id')}"/> <label for="wewrite-${$(this).data('id')}">We Write Your Ads</label> <span class="price">+ $250</span></li>
+              </ul>
+            </div>
+          </div>`;
+      }
+
       $('#cart #list').append(html);
       
       $('#checkoutButton').data('total', $('#checkoutButton').data('total') + $(this).data('price'));
@@ -44,7 +65,7 @@ var paid = 0;
         $('#' + next_id).prop('disabled', true);
       }
 
-      if ($('.cart-item').length == discount_weeks) {
+      if (($('.cart-item').length - $('.cart-item.single-day').length) == discount_weeks && !$(this).hasClass('single-day')) {
         $.each($('.cart-item'), function(i, val) {
           classes = $(val)[0]['className'];
           class_array = classes.split(' ');
@@ -61,7 +82,7 @@ var paid = 0;
           $('.total #amt').text($('#checkoutButton').data('total'));
           $('#depositButton').data('total', $('#checkoutButton').data('total') * down_payment);
         });
-      } else if ($('.cart-item').length > discount_weeks) {
+      } else if (($('.cart-item').length - $('.cart-item.single-day').length) > discount_weeks && !$(this).hasClass('single-day')) {
         id = $(this).data('id');
         item = $(`.${id}`);
 
@@ -75,10 +96,12 @@ var paid = 0;
         $('.total #amt').text($('#checkoutButton').data('total'));
         $('#depositButton').data('total', $('#checkoutButton').data('total') * down_payment);
       }
-      if ($('.cart-item').length == max_weeks) {
+      if (($('.cart-item').length - $('.cart-item.single-day').length) == max_weeks ) {
         $.each($('.purchase-checkbox'), function(i, val) {
-          if(!$(val).prop('checked')) {
-            $(val).prop('disabled', true);
+          if(!$(val).hasClass('single-day')) {
+            if(!$(val).prop('checked')) {
+              $(val).prop('disabled', true);
+            }
           }
         });
       }
